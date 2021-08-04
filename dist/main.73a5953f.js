de18c4323d02d8dff3d99e9ac8b3e765e346f505
Poyap <ryougiako@gmail.com>
2021-08-04 19:11:41 +0800

    delete .idea

--- dist/main.73a5953f.js
+++ dist/main.73a5953f.js
@@ -11417,7 +11417,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '57150' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '54900' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
